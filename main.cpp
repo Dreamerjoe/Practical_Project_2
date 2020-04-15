100e29d62d85eb12b1cbde52bf5065a7e78ecb18
Practical_Project_2 <sshwang@handong.edu>
2020-04-15 05:59:53 -0700

    hw2 done

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 int sorted[10];
 
 int main(){
+    int data[10];
+    for(int i=0;i<10;i++){
+        cin>>data[i];
+    }
+    cout<<endl;
 
+    merge_sort(data,0,9);
+
+    for(int i=0;i<10;i++){
+        cout << sorted[i]<< " ";
+    }
+    cout<<endl;
+    
     return 0;
 }
